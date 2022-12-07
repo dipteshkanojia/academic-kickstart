60d98bbf22d28e0ad34db7450c97f1d45cf840c3
Diptesh Kanojia <dipteshkanojia@gmail.com>
2022-12-07 17:04:19 +0000

    updates

--- completeUpdate.sh
+++ completeUpdate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "<footer class=\"site-footer\">
+	<p>Last Updated: $(date +"%d-%m-%Y %H:%M:%S")</p>
+	<p>\"Nearly everyone can stand adversity, but if you want to test their character, give them power.\"</p>
+</footer>" > layouts/partials/site_footer.html
+
 git add .
 git commit -m "updates"
 git push
--- view.sh
+++ view.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+echo "<footer class=\"site-footer\">
+	<p>Last Updated: $(date +"%d-%m-%Y %H:%M:%S")</p>
+	<p>\"Nearly everyone can stand adversity, but if you want to test their character, give them power.\"</p>
+</footer>" > layouts/partials/site_footer.html
+
 hugo --i18n-warnings server
